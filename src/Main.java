d58bbfd14fb2e7e397878ab2cc02a28396ce5d29
yevheniiredziuk <yevheniiredziuk@gmail.com>
2025-09-11 00:00:31 +0300

    update main with all drivers methods

--- src/Main.java
+++ src/Main.java
@@ -7,20 +7,30 @@ public class Main {
         graph.addVertex(9);
         graph.addVertex(1);
 
-        graph.removeVertex(1);
         graph.addEdge(2,4);
         graph.addEdge(2,5);
         graph.addEdge(4,9);
         graph.addEdge(5,9);
         graph.addEdge(9,2);
 
+        System.out.println("graph with all vertexes and edges: ");
+        printGraphAdjacencyList(graph);
+
+        graph.removeVertex(1);
         graph.removeEdge(2, 4);
 
+        System.out.println("graph after vertex 1 and edge [2,4] were deleted: ");
+        printGraphAdjacencyList(graph);
+
+        System.out.printf("does graph have edge [%d,%d] -> %b%n",2, 4, graph.hasEdge(2,4));
+
+        System.out.printf("does graph have vertex %d -> %b%n", 5, graph.hasVertex(5));
+
+    }
+
+    public static void printGraphAdjacencyList(Graph graph){
         graph.getAdjList()
                 .forEach((key, values)
                         -> System.out.println(key + " -> " + values));
-
-        System.out.println(graph.hasEdge(2, 4));
-        System.out.println(graph.hasVertex(5));
     }
 }
